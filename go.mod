a84e5c4be63be6890d233948d767f2c00fffcf07
tajamullone106-driod <tajamullone106@gmail.com>
2026-08-31 10:28:08 +0000

    Fix Auryvex Go foundation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tajamullone106-driod/Auryvex
 
 go 1.25.0
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/bot/bot.go
@@ -0,0 +1,40 @@
+package bot
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+	"github.com/tajamullone106-driod/Auryvex/internal/config"
+)
+
+func Start(cfg *config.Config) {
+	b, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Auryvex started as @%s", b.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := b.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() && update.Message.Command() == "start" {
+			msg := tgbotapi.NewMessage(
+				update.Message.Chat.ID,
+				"Welcome to Auryvex 🎵\n\nUse /room to create a synchronized room.",
+			)
+
+			if _, err := b.Send(msg); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
